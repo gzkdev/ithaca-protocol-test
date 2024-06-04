d532bb93696042a17a76374d6ec955f27e504ffc
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-06-04 20:14:51 +0100

    update utils, types, hooks

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,21 +1,55 @@
-export type RawPost = {
+export type CommentData = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
+export type PostData = {
   userId: number;
   id: number;
   title: string;
   body: string;
 };
 
-export type Post = {
-  body: string;
-  id: string;
+export type UserData = {
+  id: number;
   name: string;
   username: string;
-  userId: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export type Post = {
+  body: string;
+  id: number;
+  name: string;
+  username: string;
+  userId: number;
   title: string;
 };
 
 export type User = {
-  id: string;
   name: string;
+  userId: number;
   username: string;
 };
+
+export type UserStore = { [id: number]: User };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,30 @@
-import { Dispatch } from "react";
+import { Post, PostData, User, UserData, UserStore } from "./types";
 
-export async function fetchResource<T>(
-  url: string,
-  setState?: Dispatch<React.SetStateAction<T>>
-) {
+export async function fetchResource<T>(url: string): Promise<T> {
   try {
     const res = await fetch(url);
     const data = await res.json();
-    setState?.(data);
+    return data;
   } catch {
-    throw new Error("");
+    throw new Error(`Failed to fetch resource at ${url}`);
   }
+}
+
+export function getUsersStore(users: UserData[]): UserStore {
+  const store: UserStore = {};
+  users.forEach(({ id, name, username }) => {
+    store[id] = { userId: id, name, username };
+  });
+  return store;
+}
+
+function getUserDetails(users: UserStore, userId: number): User {
+  if (users[userId]) throw new Error("User doesn't exist");
+  return users[userId];
+}
+
+export function getPostsWithUsers(users: UserStore, posts: PostData[]): Post[] {
+  return posts.map(({ body, id, title, userId }) => {
+    return { body, id, title, ...getUserDetails(users, userId) };
+  });
 }
