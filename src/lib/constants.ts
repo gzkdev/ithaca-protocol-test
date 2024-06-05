e4e9651b0fbc9a3e76ed5f2a0ce129029e65c61c
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-06-05 16:28:49 +0100

    document constants and utils

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,8 @@
-export const postsBaseUrl = "https://jsonplaceholder.typicode.com/posts";
+// Base URL for fetching users data from the JSONPlaceholder API.
 export const usersBaseUrl = "https://jsonplaceholder.typicode.com/users";
+
+// Base URL for fetching posts data from the JSONPlaceholder API.
+export const postsBaseUrl = "https://jsonplaceholder.typicode.com/posts";
+
+// Base URL for fetching comments data from the JSONPlaceholder API
 export const commentsBaseUrl = "https://jsonplaceholder.typicode.com/comments";
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,17 @@
 import { Post, PostData, User, UserData, UserStore } from "./types";
 
+/**
+ * Fetches a resource from the specified URL.
+ *
+ * @template T - The expected type of the response data.
+ * @param {string} url - The URL to fetch the resource from.
+ * @param {RequestInit} [options={}] - Optional fetch options.
+ * @returns {Promise<T>} - A promise that resolves to the fetched data.
+ * @throws Will throw an error if the network response is not ok.
+ */
 export async function fetchUrlResource<T>(
   url: string,
-  options: RequestInit= {}
+  options: RequestInit = {}
 ): Promise<T> {
   const response = await fetch(url, options);
   if (!response.ok) {
@@ -12,6 +21,12 @@ export async function fetchUrlResource<T>(
   return data as T;
 }
 
+/**
+ * Converts an array of user data into a UserStore object.
+ *
+ * @param {UserData[]} users - An array of user data.
+ * @returns {UserStore} - A store object with user IDs as keys and user details as values.
+ */
 export function getUsersStore(users: UserData[]): UserStore {
   const store: UserStore = {};
   users.forEach(({ id, name, username }) => {
@@ -20,11 +35,26 @@ export function getUsersStore(users: UserData[]): UserStore {
   return store;
 }
 
+/**
+ * Retrieves the details of a user by user ID.
+ *
+ * @param {UserStore} users - The user store object.
+ * @param {number} userId - The ID of the user to retrieve.
+ * @returns {User} - The user details.
+ * @throws Will throw an error if the user does not exist in the store.
+ */
 function getUserDetails(users: UserStore, userId: number): User {
   if (!users[userId]) throw new Error("User doesn't exist");
   return users[userId];
 }
 
+/**
+ * Transforms post data by adding user details to each post.
+ *
+ * @param {UserStore} users - The user store object.
+ * @param {PostData[]} posts - An array of post data.
+ * @returns {Post[]} - An array of posts with user details included.
+ */
 export function transformPostsData(
   users: UserStore,
   posts: PostData[]
@@ -34,6 +64,12 @@ export function transformPostsData(
   });
 }
 
+/**
+ * Transforms user data into a simpler user object format.
+ *
+ * @param {UserData[]} users - An array of user data.
+ * @returns {User[]} - An array of simplified user objects.
+ */
 export function transformUserData(users: UserData[]): User[] {
   return users.map(({ id, name, username }) => {
     return { userId: id, name, username };
