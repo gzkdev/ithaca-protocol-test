a6458edc7371bce53d2f9d93ae175055f8288efe
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-06-05 12:29:56 +0100

    update post components

--- src/components/post-item.tsx
+++ src/components/post-item.tsx
@@ -1,27 +1,32 @@
-import { useEffect, useState, Suspense } from "react";
+import { useEffect, useState, MouseEvent } from "react";
 import { Comment, Post } from "../lib/types";
 import { commentsBaseUrl } from "../lib/constants";
 import { fetchUrlResource } from "../lib/utils";
+import { CloseIcon } from "../assets/icons";
 import styles from "../styles/posts-list.module.css";
 
 export default function PostItem({ post }: { post: Post }) {
   const { title, name, body } = post;
   const [fullScreenMode, setFullScreenMode] = useState(false);
 
-  function toggleFullScreenMode() {
-    setFullScreenMode((isfullScreen) => !isfullScreen);
+  function enterFullScreenMode(e: MouseEvent) {
+    e.preventDefault();
+    setFullScreenMode(true);
+  }
+
+  function leaveFullScreenMode(e: MouseEvent) {
+    e.stopPropagation();
+    setFullScreenMode(false);
   }
 
   useEffect(() => {
-    document.body.style.overflow = fullScreenMode ? "hidden" : "auto";
+    document.documentElement.style.overflow = fullScreenMode
+      ? "hidden"
+      : "auto";
   }, [fullScreenMode]);
 
   return (
-    <div
-      tabIndex={0}
-      className={styles.postItem}
-      onClick={toggleFullScreenMode}
-    >
+    <div tabIndex={0} className={styles.postItem} onClick={enterFullScreenMode}>
       <div className={styles.postTitle}>{title}</div>
       <div className={styles.postUserName}>By {name}</div>
       <div className={styles.postBody}>{body}</div>
@@ -29,7 +34,7 @@ export default function PostItem({ post }: { post: Post }) {
       {fullScreenMode && (
         <PostItemFullScreen
           post={post}
-          toggleFullScreenMode={toggleFullScreenMode}
+          leaveFullScreenMode={leaveFullScreenMode}
         />
       )}
     </div>
@@ -38,10 +43,10 @@ export default function PostItem({ post }: { post: Post }) {
 
 function PostItemFullScreen({
   post: { title, name, body, id },
-  toggleFullScreenMode,
+  leaveFullScreenMode,
 }: {
   post: Post;
-  toggleFullScreenMode(): void;
+  leaveFullScreenMode(e: React.MouseEvent): void;
 }) {
   const [comments, setComments] = useState<Array<Comment>>([]);
   const commentsUrl = `${commentsBaseUrl}?postId=${id}`;
@@ -62,7 +67,9 @@ function PostItemFullScreen({
   return (
     <div className={styles.postItemFullScreen}>
       <div className={styles.postItemFullScreenContainer}>
-        <button onClickCapture={toggleFullScreenMode}>Close</button>
+        <button title="Close" onClick={leaveFullScreenMode}>
+          <CloseIcon />
+        </button>
         <div className={styles.postItemContent}>
           <div className={styles.postTitle}>{title}</div>
           <div className={styles.postUserName}>By {name}</div>
@@ -70,9 +77,8 @@ function PostItemFullScreen({
         </div>
 
         <div className={styles.postItemComments}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <PostItemComments comments={comments} />
-          </Suspense>
+          <header>Comments</header>
+          <PostItemComments comments={comments} />
         </div>
       </div>
     </div>
@@ -81,10 +87,12 @@ function PostItemFullScreen({
 
 function PostItemComments({ comments }: { comments: Comment[] }) {
   return (
-    <div>
-      {comments.map(({ body, id, name }) => (
-        <div key={id} style={{ margin: "40px 0" }}>
-          <div>{name} commented</div>
+    <div className={styles.postItemsGrid}>
+      {comments.map(({ body, id, email }) => (
+        <div key={id} className={styles.postItemComment}>
+          <div>
+            <span>{email}</span> commented
+          </div>
           <div>{body}</div>
         </div>
       ))}
